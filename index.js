dc2a5a4b6b91f51e10de1cde9e4f6d67e900fdc7
hameedsk381 <hameedsk381@gmail.com>
2024-02-06 17:11:57 +0530

    added sharing

--- index.js
+++ index.js
@@ -16,123 +16,141 @@ app.use(bodyParser.urlencoded({ extended: true })); // Support encoded bodies
 app.use(express.json());
 const pool = require('./db');
 
-app.get('/api/renarrations', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM renarrations');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
 const upload = multer({ dest: 'sweets/' });
 const Datastore = require('nedb');
 
 const renarrationsDb = new Datastore({ filename: './sweets.db', autoload: true });
 const insertRenarration = (renarrationData, callback) => {
-    renarrationsDb.insert(renarrationData, callback);
-  };
-  const getAllRenarrations = (callback) => {
+    const sharingId = Math.floor(100000 + Math.random() * 900000); // Generate 6 digit id
+    const renarrationDataFinal = {
+        sharingId: sharingId, ...renarrationData
+    }
+    renarrationsDb.insert(renarrationDataFinal, callback);
+};
+const getAllRenarrations = (callback) => {
     renarrationsDb.find({}, callback);
-  };
-  const updateRenarration = (id, renarrationData, callback) => {
-    renarrationsDb.update({ _id: id }, { $set: renarrationData }, {}, callback);
-  };
-  const deleteRenarration = (id, callback) => {
+};
+const updateRenarration = (id, renarrationData, callback) => {
+    renarrationsDb.findOne({ _id: id }, (err, doc) => {
+        if (err) {
+            callback(err);
+        } else {
+            if (doc) {
+                renarrationsDb.update({ _id: id }, { $set: renarrationData }, {}, callback);
+            } else {
+                callback(new Error('Renarration not found'));
+            }
+        }
+    });
+};
+const deleteRenarration = (id, callback) => {
     renarrationsDb.remove({ _id: id }, {}, callback);
-  };
-  const getRenarrationById = (id, callback) => {
+};
+const getRenarrationById = (id, callback) => {
     renarrationsDb.findOne({ _id: id }, callback);
 };
-
-  app.post('/create-renarration', upload.any(), (req, res) => {
-      try {
-          let renarration = req.body  // Assuming renarration data (except media) is sent as a JSON string
-          renarration.blocks.forEach((block, index) => {
-              // Process file sweets for each block
-            if(req.files){
-                const blockFiles =  req.files.filter(file => file.fieldname === `block${index}image` || file.fieldname === `block${index}audio` || file.fieldname === `block${index}video`);
-                blockFiles.forEach(file => {
-                    if (file.fieldname === `block${index}image`) {
-                        block.image = `/sweets/${file.filename}`;
-                    } else if (file.fieldname === `block${index}audio`) {
-                        block.audio = `/sweets/${file.filename}`;
-                    } else if (file.fieldname === `block${index}video`) {
-                        block.video = `/sweets/${file.filename}`;
-                    }
-                });
-            }
-          });
-          // Insert the renarration data into the database
-          insertRenarration(renarration, (err, newDoc) => {
-              if (err) return res.status(500).send(err);
-              res.status(201).json(newDoc);
-          });
-      } catch (error) {
-          console.error('Error processing the request:', error);
-          res.status(500).send('Error processing the request');
-      }
-  });
-  
-  app.get('/renarrations/:id', (req, res) => {
-    const id = req.params.id;
-
-    getRenarrationById(id, (err, doc) => {
-        if (err) {
-            console.error('Error fetching renarration:', err);
-            return res.status(500).send('Error fetching renarration');
-        }
-
-        if (!doc) {
-            return res.status(404).send('Renarration not found');
-        }
-
-        res.json(doc);
-    });
-});
-
-  app.get('/renarrations', (req, res) => {
-    getAllRenarrations((err, docs) => {
-        if (err) return res.status(500).send(err);
-        res.json(docs);
-    });
-});
-
-app.put('/renarrations/:id', upload.any(), (req, res) => {
+app.post('/create-renarration', upload.any(), (req, res) => {
     try {
-        const id = req.params.id;
         let renarration = req.body  // Assuming renarration data (except media) is sent as a JSON string
         renarration.blocks.forEach((block, index) => {
-            const blockFiles = req.files.filter(file => file.fieldname === `block${index}image` || file.fieldname === `block${index}audio` || file.fieldname === `block${index}video`);
-            blockFiles.forEach(file => {
-                if (file.fieldname === `block${index}image`) {
-                    block.image = `/sweets/${file.filename}`;
-                } else if (file.fieldname === `block${index}audio`) {
-                    block.audio = `/sweets/${file.filename}`;
-                } else if (file.fieldname === `block${index}video`) {
-                    block.video = `/sweets/${file.filename}`;
-                }
-            });
+            // Process file sweets for each block
+          if(req.files){
+              const blockFiles =  req.files.filter(file => file.fieldname === `block${index}image` || file.fieldname === `block${index}audio` || file.fieldname === `block${index}video`);
+              blockFiles.forEach(file => {
+                  if (file.fieldname === `block${index}image`) {
+                      block.image = `/sweets/${file.filename}`;
+                  } else if (file.fieldname === `block${index}audio`) {
+                      block.audio = `/sweets/${file.filename}`;
+                  } else if (file.fieldname === `block${index}video`) {
+                      block.video = `/sweets/${file.filename}`;
+                  }
+              });
+          }
         });
-        // Update the renarration in the database
-        updateRenarration(id, renarration, (err, numReplaced) => {
+        // Insert the renarration data into the database
+        insertRenarration(renarration, (err, newDoc) => {
             if (err) return res.status(500).send(err);
-            res.json({ message: 'Renarration updated', numReplaced });
+            res.status(201).json(newDoc);
         });
     } catch (error) {
         console.error('Error processing the request:', error);
         res.status(500).send('Error processing the request');
     }
 });
-app.delete('/renarrations/:id', (req, res) => {
-    const id = req.params.id;
-    deleteRenarration(id, (err, numRemoved) => {
-        if (err) return res.status(500).send(err);
-        res.json({ message: 'Renarration deleted', numRemoved });
-    });
+
+app.get('/renarrations/:id', (req, res) => {
+  const id = req.params.id;
+
+  getRenarrationById(id, (err, doc) => {
+      if (err) {
+          console.error('Error fetching renarration:', err);
+          return res.status(500).send('Error fetching renarration');
+      }
+
+      if (!doc) {
+          return res.status(404).send('Renarration not found');
+      }
+
+      res.json(doc);
+  });
 });
-       
+
+app.get('/renarrations', (req, res) => {
+  getAllRenarrations((err, docs) => {
+      if (err) return res.status(500).send(err);
+      res.json(docs);
+  });
+});
+
+app.put('/renarrations/:id', upload.any(), (req, res) => {
+  try {
+      const id = req.params.id;
+      let renarration = req.body  // Assuming renarration data (except media) is sent as a JSON string
+      renarration.blocks.forEach((block, index) => {
+          const blockFiles = req.files.filter(file => file.fieldname === `block${index}image` || file.fieldname === `block${index}audio` || file.fieldname === `block${index}video`);
+          blockFiles.forEach(file => {
+              if (file.fieldname === `block${index}image`) {
+                  block.image = `/sweets/${file.filename}`;
+              } else if (file.fieldname === `block${index}audio`) {
+                  block.audio = `/sweets/${file.filename}`;
+              } else if (file.fieldname === `block${index}video`) {
+                  block.video = `/sweets/${file.filename}`;
+              }
+          });
+      });
+      // Update the renarration in the database
+      updateRenarration(id, renarration, (err, numReplaced) => {
+          if (err) return res.status(500).send(err);
+          res.json({ message: 'Renarration updated', numReplaced });
+      });
+  } catch (error) {
+      console.error('Error processing the request:', error);
+      res.status(500).send('Error processing the request');
+  }
+});
+app.delete('/renarrations/:id', (req, res) => {
+  const id = req.params.id;
+  deleteRenarration(id, (err, numRemoved) => {
+      if (err) return res.status(500).send(err);
+      res.json({ message: 'Renarration deleted', numRemoved });
+  });
+});  
+app.get('/verify-sharing/:sharingId', (req, res) => {
+    const sharingId = req.params.sharingId;
+  
+    renarrationsDb.findOne({ sharingId: parseInt(sharingId) }, (err, doc) => {
+      if (err) {
+        console.error('Error fetching renarration:', err);
+        return res.status(500).send('Error fetching renarration');
+      }
+  
+      if (!doc) {
+        return res.status(404).send('Renarration with the provided sharing ID not found');
+      }
+  
+      res.json(doc);
+    });
+  });     
 app.post('/download', async (req, res) => {
     const { url } = req.body;
     const device = req.headers['User-Agent'];
