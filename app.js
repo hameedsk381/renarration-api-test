7d43622781daef53b0d1df041e8bd501c5084608
hameed Shaik <hameedsk381@gmail.com>
2024-03-06 17:58:46 +0530

    cors issue solved

--- app.js
+++ app.js
@@ -12,24 +12,27 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Specify your client domain
-const allowedOrigins = ['https://renarration.onrender.com','https://renarration-ui.vercel.app'];
+// // Specify your client domain
+// const allowedOrigins = ['https://renarration.onrender.com','https://renarration-ui.vercel.app'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  // Optionally add other CORS configuration options
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   // Optionally add other CORS configuration options
+// };
 
 // Use CORS middleware for all routes
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Welcome to the Renarration API');
+});
 
 app.use('/sweets', RenarrationRouter);
 app.use('/download', fetchRouter);
